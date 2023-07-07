e8403cbe86e7cc6ea2dc6ceecb411bab95c6115a
Poojachakor <chakorpooja07@gmail.com>
2023-07-07 21:12:54 +0530

    [Pooja]Add: CensusAnalyserLive

---
+++ CensusAnalyserLive/CensusAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using CensusAnalyserLive.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CensusAnalyserLive
+{
+    public class CensusAnalyser
+    {
+        public enum Country
+        {
+            INDIA,
+        }
+
+        Dictionary<string, CensusDTO> dataMap;
+
+        public Dictionary<string, CensusDTO> LoadCensusData(Country country, string csvFilePath, string dataHeaders)
+        {
+            dataMap = new CSVAdapterFactory().LoadCsvData(country, csvFilePath, dataHeaders);
+            return dataMap;
+        }
+
+    }
+}
+
